247751bf3e332fa99fccd04beeb40f6d0eeb9592
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-03-25 20:52:10 +0100

    remove useless deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import LandingPage from "./pages/LandingPage";
 import ShowAll from "./pages/ShowAll";
 
-
-
 import { BrowserRouter ,Route,Routes,Navigate } from "react-router-dom";
-
-
-
 
 
 function App() {
@@ -21,8 +14,6 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />}></Route>
       <Route path="/ShowAll" element={<ShowAll />}></Route>
-      
-      
       
       
     </Routes>
