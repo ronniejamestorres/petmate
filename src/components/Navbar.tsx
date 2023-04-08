d898db3556b2323ba52874200c800882813ad91e
Buidlor <jefboone@gmail.com>
2023-04-08 16:14:17 +0200

    some slight styling mod in landingpage. fixed navbar and padding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,15 +7,13 @@ import logoImage from "../images/petmate-newlogo-01.svg";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 
-
-
 function Navbar() {
   const navigate = useNavigate();
   const [toggle, setToggle] = useState(false); //true open and false close
   const handleClick = () => setToggle(!toggle);
 
   return (
-    <nav className="w-full h-[80px] bg-beige1 border-b">
+    <nav className="w-full fixed h-[80px] bg-beige1 border-b shadow-md md:px-3">
       <div className="md:max-w-[1480px] max-w-[600px] m-auto w-full h-full flex justify-between items-center md:px-0 px-4">
         <img src={logoImage} alt="Logo" className="h-[25px]" />
 
@@ -27,17 +25,17 @@ function Navbar() {
             <li>Platform</li>
             <li>Pricing</li>
           </ul>
-        </div >
+        </div>
 
-        <div className="hidden md:flex">
+        <div className="hidden md:flex gap-2">
           <button
-            className="bg-beige2 hover:bg-grey px-8 py-3 rounded-full"
+            className="bg-beige2 hover:bg-grey md:px-5 md:py-2 rounded-full"
             onClick={() => navigate("/Login")}
           >
             LOGIN
           </button>
           <button
-            className="bg-beige3 hover:bg-grey px-8 py-3 rounded-full "
+            className="bg-beige3 hover:bg-grey md:px-5 md:py-2 rounded-full "
             onClick={() => navigate("/Register")}
           >
             REGISTER
@@ -49,31 +47,35 @@ function Navbar() {
         </div>
       </div>
 
-      <div className={toggle?'absolute z-10 p-4  bg-[#FFFF] w-full px-8 md:hidden border-b':'hidden'}>
+      <div
+        className={
+          toggle
+            ? "absolute z-10 p-4  bg-[#FFFF] w-full px-8 md:hidden border-b"
+            : "hidden"
+        }
+      >
         <ul>
           <li className="p-4 hover:bg-gray-100">Home</li>
           <li className="p-4 hover:bg-gray-100">About</li>
           <li className="p-4 hover:bg-gray-100">Support</li>
           <li className="p-4 hover:bg-gray-100">Platform</li>
           <li className="p-4 hover:bg-gray-100">Pricing</li>
-          <div className="flex flex-col my-4 gap-4">
-          <button
-            className="bg-beige2 px-8 py-5 rounded-full hover:bg-grey "
-            onClick={() => navigate("/Login")}
-          >
-            LOGIN
-          </button>
-          <button
-            className="bg-beige3 px-8 py-5 rounded-full hover:bg-grey "
-            onClick={() => navigate("/Register")}
-          >
-            REGISTER
-          </button>
+          <div className="flex flex-col my-4 gap-3">
+            <button
+              className="bg-beige2 px-3 py-2 rounded-full hover:bg-grey "
+              onClick={() => navigate("/Login")}
+            >
+              LOGIN
+            </button>
+            <button
+              className="bg-beige3 px-3 py-2 rounded-full hover:bg-grey "
+              onClick={() => navigate("/Register")}
+            >
+              REGISTER
+            </button>
           </div>
         </ul>
       </div>
-
-      
     </nav>
   );
 }
