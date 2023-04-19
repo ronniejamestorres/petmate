b870bab24eaa394952715c1e92a52c45e8880234
Buidlor <jefboone@gmail.com>
2023-04-19 09:38:58 +0200

    changed the socket link to live

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("http://wave.nodestarter.eu:4000");
 socket.on("connect", () => {
   console.log("front is connecting to server");
 });
