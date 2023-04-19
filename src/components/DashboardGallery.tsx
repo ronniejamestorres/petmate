08ff6788f4dcad5916bce7977987ae7fbe4ef6fd
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-04-19 19:28:08 +0200

    dashboard start data image fetching

--- src/components/DashboardGallery.tsx
+++ src/components/DashboardGallery.tsx
@@ -28,24 +28,14 @@ function DashboardGallery() {
 
   return (
     <div className="grid grid-cols-2 lg:grid-cols-2 gap-4">
-      <div className=" w-40 h-40 bg-beige3 rounded-full relative   hover:scale-125 overflow-hidden">
-        <div className="w-40 h-40  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
-          <label htmlFor="upload1">
-            {image1 ? (
-              <img src={image1} alt="Uploaded" className="w-full h-full " />
-            ) : (
-              <p className="text-4xl font-bold text-beige1">+</p>
-            )}
-          </label>
-          <input
-            type="file"
-            id="upload1"
-            style={{ display: "none" }}
-            accept="image/*"
-            onChange={handleUpload1}
-          />
+      {
+        <div className=" w-40 h-40 bg-beige3 rounded-full relative   hover:scale-125 overflow-hidden">
+          <div className="w-40 h-40  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
+            <p className="text-4xl font-bold text-beige1">+</p>
+            // here comes the image fetching from the database
+          </div>
         </div>
-      </div>
+      }
       <div className="w-40 h-40 bg-beige3 rounded-full relative   hover:scale-125 overflow-hidden">
         <div className="w-40 h-40  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center ">
           <label htmlFor="upload2">
--- src/pages/DashboardTwo.tsx
+++ src/pages/DashboardTwo.tsx
@@ -1,27 +1,29 @@
-import React from "react";
-import ContainerCardCopy from "../components/DashboardCard";
+import { useEffect, useState, useContext } from "react";
+import CardsContext from "../contexts/CardsContext";
+
 import NavbarLogin from "../components/NavbarLogin";
 import backgroundImage from "../images/petmate-background-01.svg";
 import DashboardGallery from "../components/DashboardGallery";
 import ImageDisplay from "../components/ImageDisplay";
-import { FaJedi } from "react-icons/fa";
-import axios from "axios";
-import { Dashboard } from "@mui/icons-material";
+
 import DashboardCard from "../components/DashboardCard";
 
 function DashboardTwo() {
-  const data = {};
-  axios.patch(
-    `http://wave.nodestarter.eu:4000/users/update/${localStorage.getItem(
-      "loggedIn"
-    )}`,
-    data,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const { users } = useContext(CardsContext);
+
+  const loggedInId =
+    users &&
+    users.find((user) => user._id === localStorage.getItem("loggedIn"));
+  const [picturesPaths, setpicturesPaths] = useState([]);
+
+  useEffect(() => {
+    const getPicturesPaths = () => {
+      const picturesPaths = loggedInId && loggedInId.pictures;
+      setpicturesPaths(picturesPaths);
+    };
+    getPicturesPaths();
+  }, [loggedInId]);
+
   return (
     <div
       className="h-screen bg-center bg-repeat  bg-beige1 overflow-auto "
@@ -33,7 +35,7 @@ function DashboardTwo() {
       <div className=" flex items-center justify-center h-screen  ">
         <div className=" grid grid-cols-1 lg:grid-cols-2">
           <div className="  col-span-1 lg:col-span-1 bg-beige2  p-4 rounded-xl shadow-2xl">
-            <DashboardGallery />
+            <DashboardGallery picturesPaths={picturesPaths} />
           </div>
 
           <div className=" col-span-1 lg:col-span-1 bg-white  p-4 rounded-xl shadow-2xl">
